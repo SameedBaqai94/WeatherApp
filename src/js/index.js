96aea829c9d96e03b060c7d3d442396ff365dcdf
sameedbaqai94 <sameedbaqai94@gmail.com>
2022-04-27 01:09:21 -0400

    Webpack and fetch for location added

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,19 @@
 'use strict';
 
+const WeatherApp = () => {
+    const API_KEY = 'a665874f32d3fac7e4257030076e571e';
+    const API = 'http://api.openweathermap.org/';
+
+    const getCityLatLong = (city) => {
+        fetch(`${API}/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`, {
+            mode: 'cors',
+        })
+            .then((response) => response.json())
+            .then((data) => console.log(data))
+    }
+
+    return { getCityLatLong }
+}
+
+const weather = WeatherApp();
+weather.getCityLatLong('Toronto');
