97c8723feb59cf37c9bb14dd45a74e957086f496
sameedbaqai94 <SameedB@ppfd.com>
2022-04-27 16:00:11 -0400

    added promise and fetch

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,48 @@
 'use strict';
 
 const WeatherApp = () => {
-    const API_KEY = 'a665874f32d3fac7e4257030076e571e';
+    const API_KEY = '1150b4611575b91fbe90116a52b03265';
     const API = 'http://api.openweathermap.org/';
 
     const getCityLatLong = (city) => {
-        fetch(`${API}/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`, {
+        return fetch(`${API}/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`, {
             mode: 'cors',
         })
             .then((response) => response.json())
-            .then((data) => console.log(data))
+            .then((data) => data)
     }
 
-    return { getCityLatLong }
+    const getTemp = (lat, long) => {
+        return fetch(`${API}data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}`, {
+            mode: 'cors'
+        })
+            .then(res => res.json())
+            .then(data => data)
+    }
+
+    const runApp = (callback, time) => {
+        return new Promise((resolve, reject) => {
+            if (true) {
+                setTimeout(() => resolve(callback), time)
+            }
+        })
+    }
+    return { runApp, getCityLatLong, getTemp }
 }
 
+
 const weather = WeatherApp();
-weather.getCityLatLong('Toronto');
+let data = null;
+weather.runApp(
+    weather.getCityLatLong('Toronto')
+        .then((res) => data = res),
+    2000)
+    .then(() => {
+        return weather.runApp(
+            weather.getTemp(data[0].lat, data[0].lon)
+                .then((res) => data = res),
+            2000
+        )
+    }).then(() => {
+        console.log(data)
+    })
